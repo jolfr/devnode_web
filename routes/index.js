87ae88a50df3356cf20fa0b97bcbcc36c1a3888e
jolfr <thomas.jack.carroll@gmail.com>
2018-11-08 15:23:07 -0500

    trying to get google sign in to display user id

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,3 @@ router.post('/myprofile', function(req, res) {
 });
 
 module.exports = router;
-
-function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    document.getElementById("user-id").innerHTML = id_token;
-
-}
