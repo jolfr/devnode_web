d85dc9a0b23a869393c88c54343d148f3cc51500
jolfr <thomas.jack.carroll@gmail.com>
2018-11-21 09:20:07 -0500

    added fields to token page

--- routes/token.js
+++ routes/token.js
@@ -1,12 +1,10 @@
 var express = require('express');
-const axios = require('axios');
-const NodeCache = require('node-cache');    //cache for session token, see npm node-cache
 const path = require("path");
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/token.html'));
 });
 
 module.exports = router;
