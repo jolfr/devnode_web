f911ba7e31d3de743510195f7a015839d44957c1
jolfr <thomas.jack.carroll@gmail.com>
2018-11-28 17:29:52 -0500

    deleted layout.css which was overriding other css already written

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import FontAwesome from '../../css/font-awesome.css'
 import JCarousel from '../../css/jcarousel.css'
 import Overwrite from '../../css/overwrite.css'
 import Style from '../../css/style.css'
+import Theme from '../../css/skins/devnode.css'
 
 const IndexPage = () => (
     <Layout>
@@ -348,91 +349,6 @@ const IndexPage = () => (
                     </div>
                 </div>
             </section>
-            <footer>
-                <div className="container">
-                    <div className="row">
-                        <div className="span3">
-                            <div className="widget">
-                                <h5 className="widgetheading">Browse pages</h5>
-                                <ul className="link-list">
-                                    <li><a href="#">About our company</a></li>
-                                    <li><a href="#">Our services</a></li>
-                                    <li><a href="#">Meet our team</a></li>
-                                    <li><a href="#">Explore our portfolio</a></li>
-                                    <li><a href="#">Get in touch with us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="span3">
-                            <div className="widget">
-                                <h5 className="widgetheading">Important stuff</h5>
-                                <ul className="link-list">
-                                    <li><a href="#">Press release</a></li>
-                                    <li><a href="#">Terms and conditions</a></li>
-                                    <li><a href="#">Privacy policy</a></li>
-                                    <li><a href="#">Career center</a></li>
-                                    <li><a href="#">Flattern forum</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="span3">
-                            <div className="widget">
-                                <h5 className="widgetheading">Flickr photostream</h5>
-                                <div className="flickr_badge">
-                                    <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"/>
-                                </div>
-                                <div className="clear">
-                                </div>
-                            </div>
-                        </div>
-                        <div className="span3">
-                            <div className="widget">
-                                <h5 className="widgetheading">Get in touch with us</h5>
-                                <address>
-                                    <strong>Flattern studio, Pte Ltd</strong><br/>
-                                    Springville center X264, Park Ave S.01<br/>
-                                    Semarang 16425 Indonesia
-                                </address>
-                                <p>
-                                    <i className="icon-phone"/> (123) 456-7890 - (123) 555-7891 <br/>
-                                    <i className="icon-envelope-alt"/> email@domainname.com
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div id="sub-footer">
-                    <div className="container">
-                        <div className="row">
-                            <div className="span6">
-                                <div className="copyright">
-                                    <p>
-                                        <span>&copy; Flattern - All right reserved.</span>
-                                    </p>
-                                    <div className="credits">
-                                        {/*
-                      All the links in the footer should remain intact.
-                      You can delete the links only if you purchased the pro version.
-                      Licensing information: https://bootstrapmade.com/license/
-                      Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Flattern
-                    */}
-                                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="span6">
-                                <ul className="social-network">
-                                    <li><a href="#" data-placement="bottom" title="Facebook"><i className="icon-facebook icon-square"/></a></li>
-                                    <li><a href="#" data-placement="bottom" title="Twitter"><i className="icon-twitter icon-square"/></a></li>
-                                    <li><a href="#" data-placement="bottom" title="Linkedin"><i className="icon-linkedin icon-square"/></a></li>
-                                    <li><a href="#" data-placement="bottom" title="Pinterest"><i className="icon-pinterest icon-square"/></a></li>
-                                    <li><a href="#" data-placement="bottom" title="Google plus"><i className="icon-google-plus icon-square"/></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
         </div>
         <a href="#" className="scrollup"><i className="icon-chevron-up icon-square icon-32 active"/></a>
         {/* javascript
