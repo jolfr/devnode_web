bee3cc7f63bbff9ce927f2d240faf83afde2a857
jolfr <thomas.jack.carroll@gmail.com>
2018-11-28 17:10:12 -0500

    moved header to component declaration

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
+import Animate from '../../css/animate.css'
+import Bootstrap from '../../css/bootstrap.css'
+import BootstrapResponsive from '../../css/bootstrap-responsive.css'
+import CSlider from '../../css/cslider.css'
+import CustomFonts from '../../css/custom-fonts.css'
+import FontAwesome from '../../css/font-awesome.css'
+import JCarousel from '../../css/jcarousel.css'
+import Overwrite from '../../css/overwrite.css'
+import Style from '../../css/style.css'
+
 
 
 import Header from './header'
