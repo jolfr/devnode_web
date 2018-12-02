ce61f9752cdde3a5237eb60d02bcd5636b3b52f7
jolfr <thomas.jack.carroll@gmail.com>
2018-12-02 13:34:28 -0500

    fixed links for listings and profile

---
+++ src/pages/listings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const ListingsPage = () => (
+    <Layout>
+        <h1>LISTINGS PAGE</h1>
+    </Layout>
+);
+
+export default ListingsPage
---
+++ src/pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const ProfilePage = () => (
+    <Layout>
+        <h1>PROFILE PAGE</h1>
+    </Layout>
+);
+
+export default ProfilePage
